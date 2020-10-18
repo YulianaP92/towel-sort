ecb2bc0ca230b681c4f07a63c2f01ce5d94e576a
Yuliana Polukhina <polushka.yulia@gmail.com>
2020-10-18 20:33:58 +0300

    feat: add sort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  
+  let result=[];
+  let third='';
+  if(matrix && matrix.length!==0){
+  let first = matrix[0];
+  let second = matrix[1];
+  if(matrix[2]){
+  third = matrix[2];
+  }
+  else{
+  third ='';
+  }
+
+  let concat = [];
+  for (let i = second.length-1; i >= 0; i--) {
+    concat.push(second[i]);
+  }
+
+  if(matrix.length===2){
+    result = [].concat(first,concat );
+  }
+  else{
+  result = [].concat(first,concat,third );
+  }
+}
+  return result;
 }
